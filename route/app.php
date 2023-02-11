4b50f84728a3cd0fa7c1bf81a01b04ecd5935ca1
wanganlin <inbox@wanganlin.com>
2023-02-11 22:01:19 +0800

    增加自动化路由

--- route/app.php
+++ route/app.php
@@ -1,9 +1,21 @@
 <?php
 
 use think\facade\Route;
+use think\helper\Str;
 
 Route::get('think', function () {
     return 'hello,ThinkPHP6!';
 });
 
-Route::get('hello/:name', 'index/hello');
+// 自动化路由
+$dirs = glob(app_path('controller/*'), GLOB_ONLYDIR);
+foreach ($dirs as $dir) {
+    $m = basename($dir);
+    $v = $m === config('app.default_app') ? '' : $m;
+    Route::group($v, function () {
+        Route::get(':c/:a', ':c/:a');
+        Route::post(':c/:a', ':c/:aHandle');
+        Route::get(':c', ':c/index');
+        Route::get('/', 'Index/index');
+    })->prefix($m.'.');
+}
